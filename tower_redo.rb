4b681c3a002da0cfaf7d84e685dd9c21a998e7b8
Alex Lach <alexglach@gmail.com>
2016-06-23 10:51:53 -0400

    all done except if you put in an error on the first entry

--- tower_redo.rb
+++ tower_redo.rb
@@ -12,6 +12,7 @@ end
 #set up towers
 towers = [(1..(number_of_disks.to_i)).to_a.reverse, [], []]
 
+#Needed methods
 
 #establish criteria for victory
 def check_for_victory(towers, number_of_disks, victory)
@@ -25,10 +26,12 @@ def check_for_victory(towers, number_of_disks, victory)
   end
 end
 
+#execute user's move
 def make_user_move(user_move_array, towers)
   towers[user_move_array[1]] << towers[user_move_array[0]].pop
 end
 
+#convert user's input to array
 def make_user_move_array (user_move, user_move_array)
   user_move.split(",").each do |input|
     user_move_array << input.to_i - 1
@@ -43,25 +46,11 @@ def check_user_quit(user_move)
   end
 end
 
-#Validate user move for format
-def user_move_format(user_move_array, user_move_valid)
-  #if there are more or less than two inputs, invalid input
-  if user_move_array.length != 2
-    return user_move_valid = false
-  #if either input is an integer, invalid input
-  end
-end
-  
 
-def user_move_rules(user_move_array, towers)
-  #if moving disk is larger than top disk on receiving tower, invalid input
-  #if moving disk is from tower that does not exist, invalid input
-  #if receiving tower does not exist, invalid input
-end
 
 def play_game(number_of_disks, towers)
   victory = false
-  print "To move the top disk in one tower to the top of another tower, please enter your move in the format 1,3 with the first number being the tower to move from and the second number being the tower to move to." + "\n"
+  print "To move the top disk in one tower to the top of another tower, please enter your move in the format 1,3 with the first number being the tower to move from and the second number being the tower to move to. Enter 'q' to exit the game." + "\n"
   #While victory criteria is not met
   while victory == false
     
@@ -74,31 +63,40 @@ def play_game(number_of_disks, towers)
     print "Tower 3: " + towers[2].to_s + "\n"
    
     
-    #Ask for user's next move
-    puts "Whats your next move?" 
-    print "> "
-    user_move = gets.chomp
-    user_move_array = []
-    #make user input readable
-    make_user_move_array(user_move, user_move_array)
-    
-    #check if user wants to quit?
-    check_user_quit(user_move)
-    
     #Validate move for game rules
     errors = 1
-    until errors == 0
-      if towers[user_move_array[0]][-1] < towers[user_move_array[1]][-1] 
-        errors -= 1
-      else
-        puts "The disk you're trying to move is larger than the top disk on the tower you're trying to move to. Please try again"
+    while errors > 0
+      #Ask for user's next move
+      
+      puts "Whats your next move?" 
+      print "> "
+      user_move = gets.chomp
+      user_move_array = []
+      #make user input readable
+      make_user_move_array(user_move, user_move_array)
+      
+      #check if user wants to quit?
+      check_user_quit(user_move)
+      possible_towers = (0..2).to_a
+      
+      until (possible_towers.include?(user_move_array[0].to_i) && possible_towers.include?(user_move_array[1].to_i)) && (towers[user_move_array[0]].empty? == false) && (towers[user_move_array[1]].empty? || towers[user_move_array[1]][-1].to_i > towers[user_move_array[0]][-1].to_i)
+        print "Tower 1: " + towers[0].to_s
+        print "Tower 2: " + towers[1].to_s
+        print "Tower 3: " + towers[2].to_s + "\n"
+        puts "There was an error in your input. Please try again."
+        puts "Whats your next move?" 
+        print "> "
         user_move = gets.chomp
         user_move_array = []
         #make user input readable
         make_user_move_array(user_move, user_move_array)
+        #check if user wants to quit?
+        check_user_quit(user_move)
       end
+      errors -= 1
+      
+      
     end
-    
     #Make User Move
     make_user_move(user_move_array, towers)
     
