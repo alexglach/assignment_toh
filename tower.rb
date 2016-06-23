82e2b70b7eef0a1d95c0d0ae3d98cac2681f94c7
Alex Lach <alexglach@gmail.com>
2016-06-23 12:13:19 -0400

    render updated

--- tower.rb
+++ tower.rb
@@ -14,15 +14,12 @@ puts "You're playing with #{number_of_disks} disks! Good luck.\n"
 #set up towers
 towers = [(1..(number_of_disks.to_i)).to_a.reverse, [], []]
 
-#Needed methods
+#METHODS
 
 #establish criteria for victory
 def check_for_victory(towers, number_of_disks, victory)
   if towers[1].join == (1..(number_of_disks.to_i)).to_a.reverse.join || towers [2].join == (1..(number_of_disks.to_i)).to_a.reverse.join
-    puts "Current Board:"
-    print "Tower 1: " + towers[0].to_s
-    print "Tower 2: " + towers[1].to_s
-    print "Tower 3: " + towers[2].to_s
+    render(towers, number_of_disks)
     puts "You win! Thanks for playing"
     exit
   end
@@ -42,12 +39,32 @@ end
 
 #check if user wants to quit
 def check_user_quit(user_move)
-  if user_move == "q" || user_move == "Q"
-    puts "Thanks for playing! See you next time"
+  if user_move == "q" || user_move == "quit"
+    puts "Thanks for playing! See you next time."
     exit
   end
 end
 
+#display current board
+def render(towers, number_of_disks)
+  puts "Current Board:"
+  counter = number_of_disks - 1
+  while counter >= 0
+    tower_counter = 0
+    while tower_counter < 3
+      if towers[tower_counter][counter] != nil
+        printing_amount = towers[tower_counter][counter].to_i
+        print ("#" *  printing_amount) + " " * (number_of_disks -  printing_amount + 1)
+      else
+        print " " * (number_of_disks + 1)
+      end
+      tower_counter += 1
+    end
+    puts "\n"
+    counter -=1
+  end
+  puts "1".ljust(number_of_disks + 1) + "2".ljust(number_of_disks + 1) + "3".ljust(number_of_disks + 1)
+end
 
 
 def play_game(number_of_disks, towers)
@@ -55,62 +72,35 @@ def play_game(number_of_disks, towers)
   print "To move the top disk in one tower to the top of another tower, please enter your move in the format 1,3 with the first number being the tower to move from and the second number being the tower to move to. Enter 'q' to exit the game." + "\n"
   #While victory criteria is not met
   while victory == false
-    
-   
-    #Display current board
-    #begin
-    puts "Current Board:"
-    print "Tower 1: " + towers[0].to_s
-    print "Tower 2: " + towers[1].to_s
-    print "Tower 3: " + towers[2].to_s + "\n"
+    render(towers, number_of_disks)
    
     
-    #Validate move for game rules
+    #Get move and validate move for format and game rules
     errors = 1
     while errors > 0
-      #Ask for user's next move
-      
+    
       puts "Whats your next move?" 
       print "> "
       user_move = gets.chomp
       user_move_array = []
-      #make user input readable
       make_user_move_array(user_move, user_move_array)
-      
-      #check if user wants to quit?
       check_user_quit(user_move)
+
       possible_towers = (0..2).to_a
-      
       until (possible_towers.include?(user_move_array[0].to_i) && possible_towers.include?(user_move_array[1].to_i)) && (towers[user_move_array[0]].empty? == false) && (towers[user_move_array[1]].empty? || towers[user_move_array[1]][-1].to_i > towers[user_move_array[0]][-1].to_i)
-        print "Tower 1: " + towers[0].to_s
-        print "Tower 2: " + towers[1].to_s
-        print "Tower 3: " + towers[2].to_s + "\n"
-        puts "There was an error in your input. Remember to put your input in X,X format. Please try again."
+        puts "There was an error in your input. Remember to enter your input in X,X format. Please try again."
         puts "Whats your next move?" 
         print "> "
         user_move = gets.chomp
         user_move_array = []
-        #make user input readable
         make_user_move_array(user_move, user_move_array)
-        #check if user wants to quit?
         check_user_quit(user_move)
       end
       errors -= 1
       
-      
     end
-    #Make User Move
     make_user_move(user_move_array, towers)
-    
-    #error for all moves
-    #rescue
-      #puts "You made an error in your input! Please try again"
-      #retry
-    #end
-    
 
-    
-    #Check for victory
     check_for_victory(towers, number_of_disks, victory)
 
   end
